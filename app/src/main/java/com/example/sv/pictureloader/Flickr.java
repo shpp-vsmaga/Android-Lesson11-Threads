4826da21ee4e3f2be38e0445a7d7e56ac1b0ddc1
shpp-vsmaga <cfunsv10@gmail.com>
2016-05-11 21:08:03 +0300

    little fixes

--- app/src/main/java/com/example/sv/pictureloader/Flickr.java
+++ app/src/main/java/com/example/sv/pictureloader/Flickr.java
@@ -7,7 +7,6 @@ import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-//import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;

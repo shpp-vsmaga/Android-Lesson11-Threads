f2dbae3363279634e0078cb704fdc3fe6ad1bc0f
shpp-vsmaga <cfunsv10@gmail.com>
2016-05-12 23:47:34 +0300

    added progressbar

--- app/src/main/java/com/example/sv/pictureloader/MainActivity.java
+++ app/src/main/java/com/example/sv/pictureloader/MainActivity.java
@@ -3,24 +3,32 @@ package com.example.sv.pictureloader;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -41,15 +49,24 @@ public class MainActivity extends AppCompatActivity {
     private volatile int downloadedPictures = 0;
     private volatile int picturesInQueue = 0;
     private String progressText;
+    private volatile long currentPictureLoadTime = 0;
+    private String minutesStr;
+    private String secondsStr;
+    private String timeLeftStr;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         edtRequest = (EditText)findViewById(R.id.edtRequest);
+        addEnterListener();
         leftPictureLayout = (LinearLayout) findViewById(R.id.leftImages);
         rightPictureLayout = (LinearLayout) findViewById(R.id.rightImages);
+        minutesStr = getResources().getString(R.string.minutes);
+        secondsStr = getResources().getString(R.string.seconds);
+        timeLeftStr = getResources().getString(R.string.timeLeft);
         btnSearch = (Button)findViewById(R.id.btnSearch);
         progressText = getResources().getString(R.string.downloading);
         threadPool = new ThreadPoolExecutor(MIN_POOL_SIZE,
@@ -60,16 +77,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initSnackBar() {
-        snackbar = Snackbar.make(btnSearch, progressText, Snackbar.LENGTH_INDEFINITE)
-                .setAction("Action", null);
+        snackbar = Snackbar.make(btnSearch, progressText, Snackbar.LENGTH_INDEFINITE);
         Snackbar.SnackbarLayout snackbarLayout = (Snackbar.SnackbarLayout) snackbar.getView();
-        snackbarLayout.addView(new ProgressBar(this));
+        snackbarLayout.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        ProgressBar progressBar = new ProgressBar(this);
+        progressBar.getIndeterminateDrawable().setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_IN);
+        snackbarLayout.setGravity(Gravity.CENTER);
+        snackbarLayout.addView(progressBar);
         snackbar.show();
     }
 
+    private void addEnterListener(){
+        edtRequest.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE){
+                    searchPictures();
+                }
+                return false;
+            }
+        });
+    }
 
     public void btnSearchOnClick(View view) {
+        hideKeyboard();
+        searchPictures();
 
+    }
+
+    private void hideKeyboard() {
+        View view = getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+
+    private void searchPictures(){
         String requestStr = edtRequest.getText().toString();
 
         if (connectionIsAvailable()){
@@ -85,9 +130,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
-
-
-
 
     public void loadPictures(ArrayList<String> urls) {
         picturesInQueue = urls.size();
@@ -107,13 +149,19 @@ public class MainActivity extends AppCompatActivity {
     private void loadPictureFromUrl(String picUrl) {
         Bitmap result = null;
         InputStream inputStream;
-
+        long beginTIme;
         try {
+            beginTIme = System.currentTimeMillis();
             URL url = new URL(picUrl);
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
             inputStream = urlConnection.getInputStream();
             result = BitmapFactory.decodeStream(inputStream);
             urlConnection.disconnect();
+
+            /*Emulate "Hard work"*/
+            Thread.sleep(200);
+
+            currentPictureLoadTime = System.currentTimeMillis() - beginTIme;
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -172,9 +220,18 @@ public class MainActivity extends AppCompatActivity {
             if (snackbar == null || !snackbar.isShown()){
                 initSnackBar();
             }
+            long timeLeft = currentPictureLoadTime * (picturesInQueue - downloadedPictures);
+            String timeStr = String.format(Locale.getDefault(), "%d %s: %d %s",
+                    TimeUnit.MILLISECONDS.toMinutes(timeLeft),
+                    minutesStr,
+                    TimeUnit.MILLISECONDS.toSeconds(timeLeft) -
+                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(timeLeft)),
+                    secondsStr);
 
             snackbar.setText(progressText + String.valueOf(downloadedPictures)
-                    + "/" + String.valueOf(picturesInQueue) );
+                    + "/" + String.valueOf(picturesInQueue) + "\n"
+                    + timeLeftStr + timeStr);
+
             if (downloadedPictures == picturesInQueue){
                 snackbar.dismiss();
                 picturesInQueue = downloadedPictures = 0;
